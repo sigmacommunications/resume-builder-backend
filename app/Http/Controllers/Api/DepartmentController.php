9d626a13176c268dfc4880af1fc7f8ad19755933
saadkhan3977 <saadkhan3977@gmail.com>
2025-08-22 15:18:47 +0500

    department update

--- app/Http/Controllers/Api/DepartmentController.php
+++ app/Http/Controllers/Api/DepartmentController.php
@@ -53,18 +53,18 @@ class DepartmentController extends BaseController
 	
 	public function update(Request $request, $id)
 	{
-		$validator = Validator::make($request->all(), [
-			'department_name' => 'required|string',
-			'department_type' => 'required|string',
-			'lead_full_name' => 'required|string',
-			'lead_email_address' => 'required|email',
-			'lead_contact_number' => 'required|string',
-			'number_of_employees_in_depart' => 'required|integer',
-		]);
+		// $validator = Validator::make($request->all(), [
+		// 	'department_name' => 'required|string',
+		// 	'department_type' => 'required|string',
+		// 	'lead_full_name' => 'required|string',
+		// 	'lead_email_address' => 'required|email',
+		// 	'lead_contact_number' => 'required|string',
+		// 	'number_of_employees_in_depart' => 'required|integer',
+		// ]);
 
-		if ($validator->fails()) {
-			return $this->sendError($validator->errors()->first());
-		}
+		// if ($validator->fails()) {
+		// 	return $this->sendError($validator->errors()->first());
+		// }
 
 		$data = $validator->validated();
 		$company = Company::where('user_id', Auth::id())->first();
@@ -77,7 +77,6 @@ class DepartmentController extends BaseController
 		}
 
 		if ($request->hasFile('image')) {
-			// पुरानी इमेज delete भी कर सकते हैं अगर चाहिए तो
 			if (!empty($department->image) && file_exists(public_path($department->image))) {
 				unlink(public_path($department->image));
 			}
