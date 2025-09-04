eb74d612b42cf3ba25f2cb0ecca52ef95434cf68
saadkhan3977 <saadkhan3977@gmail.com>
2025-09-04 16:15:31 +0500

    template purchase

--- config/services.php
+++ config/services.php
@@ -30,5 +30,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'stripe' => [
+        'STRIPE_SECRET' => env('STRIPE_SECRET'),
+        'STRIPE_PUBLISHABLE_KEY'    => env('STRIPE_KEY'),
+    ],
 
 ];
